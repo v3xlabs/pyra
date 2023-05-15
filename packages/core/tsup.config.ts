ecefcba6cb693fda32e677a813e8fc52a63f70d3
Lucemans <luc@lucemans.nl>
2023-05-15 19:41:31 +0000

    Introduce core build configs

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,18 @@
+// the best tsup config ever for browser compatability
+import type { Options } from 'tsup';
+
+const config: Options = {
+    entryPoints: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    sourcemap: true,
+    splitting: true,
+    clean: true,
+    minify: true,
+    legacyOutput: true,
+    target: 'es2018',
+    tsconfig: 'tsconfig.json',
+    external: []
+};
+
+export default config;
